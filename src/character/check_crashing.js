fb0464c2945d9b55121aeec1fd66f5699947e7dc
aleko2008GOA <alexmelikjanian@gmail.com>
2024-12-08 22:50:36 +0400

    new

--- src/character/check_crashing.js
+++ src/character/check_crashing.js
@@ -28,18 +28,18 @@ function check_crashing(character_position, iceberg_grid, speed, isStunned, isIm
     if(check_rects || character_position.x < 50 || character_position.x > 2900 || character_position.y > 2900){
         // back flipp
         if(speed.left > speed.right){
-            speed.right = speed.left > 1 ? speed.left + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
+            speed.right = speed.left > parameters.charMaxSpeed60FPS * deltaStamp / 2 ? speed.left + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
             speed.left = 0;
         }else{
-            speed.left = speed.right > 1 ? speed.right + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
+            speed.left = speed.right > parameters.charMaxSpeed60FPS * deltaStamp / 2 ? speed.right + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
             speed.right = 0;
         }
 
         if(speed.up > speed.down){
-            speed.down = speed.up > 1 ? speed.up + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
+            speed.down = speed.up > parameters.charMaxSpeed60FPS * deltaStamp / 2 ? speed.up + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
             speed.up = 0;
         }else{
-            speed.up = speed.down > 1 ? speed.down + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
+            speed.up = speed.down > parameters.charMaxSpeed60FPS * deltaStamp / 2 ? speed.down + parameters.charDeltaSpeed60FPS * deltaStamp ** 2 : parameters.charMaxSpeed60FPS * deltaStamp / 2;
             speed.down = 0;
         }
 
